6e211f5043cab370dd7c78c583475bf65ffb5ca0
Perry Cate <NPCate01@gmail.com>
2018-09-21 16:15:22 -0400

    ;lasdkfp ouq aew5

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-# Says hello to the given subject
-def say_hello(subject = "World"):
-    print("hello, {}!".format(subject))
-
-say_hello()
-say_hello("ACM")
+# Says hello tsdfo the given subject
+def say_hello(subsdfject = "World"):
+    print(sdfk"hello, {}!".format(subject))
+sdfkaskdfk
+say_iiihello()
+say_hello("ACMksdkj")
+:wq :wq X X C-M -CQ How do i exit
